75d532f1a708af2be45b32da54daa89772eb1477
gablan <gablan@kth.se>
2023-03-15 14:54:28 +0100

    Reducers added

--- src/features/user/userSlice.jsx
+++ src/features/user/userSlice.jsx
@@ -10,7 +10,15 @@ const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        
+        addMovieToList(state, action){
+            state.movieList.unshifts(action.payload);
+        },
+        addStreamingService(state, action){
+            state.streamingServices.push(action.payload);
+        },
+        removeStreamingService(state, action){
+            
+        }
     }
 })
 export const getUserName = (state) => state.user.userName;
