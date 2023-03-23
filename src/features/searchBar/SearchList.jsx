3edf18781d5230ea6a1dd2bdf78904958ad504c7
gablan <gablan@kth.se>
2023-03-23 11:49:18 +0100

    Small change

--- src/features/searchBar/SearchList.jsx
+++ src/features/searchBar/SearchList.jsx
@@ -16,9 +16,9 @@ const SearchList = () => {
         content = <img src="https://codemyui.com/wp-content/uploads/2017/09/rotate-pulsating-loading-animation.gif"/>
     } else if(status === 'succeeded'){
         //Spreads the results array and sorts it by imdb rating
-        const sortedResults = [...results].sort((a, b) => {return b.imdbRating - a.imdbRating});
+        //const sortedResults = [...results].sort((a, b) => {return b.imdbVoteCount - a.imdbVoteCount});
         //Render all movies in sortedResults using map and the rendermovie function and with imdbId as key for each rendered object
-        content = sortedResults.map(result => <RenderMovie key={result.imdbId} result={result} />);
+        content = results.map(result => <RenderMovie key={result.imdbId} result={result} />);
     } else if(status === 'failed'){
         content = <div>{error}</div>;
     }
