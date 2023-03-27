d3a495eba397dc58773e49f67e975cb3b2c610ce
GamingBreaker <gablan@kth.se>
2023-03-27 16:58:41 +0200

    Persistance added for movie list

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,21 @@ import "./index.css";
 import { store } from "./model/store";
 import { Provider } from "react-redux";
 //import { fetchCountries } from './features/searchBar/searchSlice';
-import { getServices } from "./features/userPage/userPageSlice";
+import Firebase from "./firebase/Firebase";
+import { getServices } from "./features/userPage/userPageSlice"; 
+import FirebaseProvider from './firebase/Firebase'
+
 
 //Uses getServices from userPageSlice to fetch all streaming services from the API once during page load
 store.dispatch(getServices());
+//fetchFromFirebase();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <FirebaseProvider>
+        <App />
+      </FirebaseProvider>
     </Provider>
   </React.StrictMode>
 );
