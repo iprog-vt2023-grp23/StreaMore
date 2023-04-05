2bef0679f56bdcb1f7ce40eba9bf001788431215
GamingBreaker <gablan@kth.se>
2023-04-06 00:22:34 +0200

    ApiKey removed from gitignore

---
+++ src/ApiKey.jsx
@@ -0,0 +1,10 @@
+const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'f070a4eaefmsh6a2cea4e901d65bp1dac56jsne1ade9b4f78d',
+      'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+    }
+  };
+const sourceUrl = 'https://streaming-availability.p.rapidapi.com/';
+
+export {options, sourceUrl};
