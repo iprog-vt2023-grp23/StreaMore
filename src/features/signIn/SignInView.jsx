17a5dea2c00032dfe7538c9bcbaef3884116dc64
Martin Lindefors <lindefor@kth.se>
2023-05-03 17:23:01 +0200

    Made sign-in page look better, see issue #49

--- src/features/signIn/SignInView.jsx
+++ src/features/signIn/SignInView.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
 import "./SignIn.css"
 
 const SignInView = (props) => {
@@ -25,36 +26,28 @@ const SignInView = (props) => {
   return (
     <div className="signIn">
       <h1>Sign In Or Register</h1>
-      <form>
+      <form className="inputBoxes">
         <span className="p-float-label">
-          <InputText id="username" value={props.username} onChange={usernameChanged} onKeyDown={keyDown}/>
+          <InputText id="username" className="userInput" value={props.username} onChange={usernameChanged} onKeyDown={keyDown}/>
           <label htmlFor="username">Username</label>
         </span>
         <span className="p-float-label">
-          <InputText id="e-mail" value={props.email} onChange={emailChanged} onKeyDown={keyDown}/>
+          <InputText id="e-mail" className="userInput" value={props.email} onChange={emailChanged} onKeyDown={keyDown}/>
           <label htmlFor="e-mail">E-mail</label>
         </span>
         <span className="p-float-label">
-          <InputText id="password" type="password" value={props.password} onChange={passwordChanged} onKeyDown={keyDown}/>
+          <InputText id="password" className="userInput" type="password" value={props.password} onChange={passwordChanged} onKeyDown={keyDown}/>
           <label htmlFor="password">Password</label>
         </span>
       </form>
-      <button
-        onClick={(event) => {
+      <Button label="Sign in" icon="pi pi-sign-in" raised onClick={(event) => {
           event.preventDefault();
           login();
-        }}
-      >
-        Sign In
-      </button>
-      <button
-        onClick={(event) => {
+        }}/>
+      <Button label="Register" icon="pi pi-info-circle" raised onClick={(event) => {
           event.preventDefault();
           register();
-        }}
-      >
-        Register
-      </button>
+        }}/>
     </div>
   );
 };
