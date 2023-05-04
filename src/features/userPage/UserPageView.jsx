d38ef87e389e0f2ad01495bfd5ce58d1b6cc0579
Michael Arenander <michael.arenander@gmail.com>
2023-05-04 12:53:47 +0200

    refactoring userpage view code

--- src/features/userPage/UserPageView.jsx
+++ src/features/userPage/UserPageView.jsx
@@ -5,24 +5,19 @@ import { Button } from 'primereact/button';
 import "./UserPage.css";
 
 const UserPageView = (props) => {
-  const keyDown = (e) => props.onKeyDown(e);
-  const onToggleEditmode = (e) => props.onEdit();
-  const usernameChanged = (e) => {
-    console.log("usernameChanged triggered!\n action-object:", e);
-    props.onUsernameChanged(e);
-  };
+  const keyDown = e => props.onKeyDown(e);
+  const onToggleEditmode = e => props.onEdit();
+  const usernameChanged = e => props.onUsernameChanged(e);
 
-
-  console.log("props ", props);
+  //console.log("props ", props);
   /*
    *Renders all services that are retrieved from the api
    */
   const renderStreamingServices = props.services.map((service) => {
     //If service is in streamingServices (the users owned services) it will be rendered with the css class OwnedService, giving it a red circle
     //The icons are fetched from StreamingButtons
-    //console.log("service: ", service);
+
     const hasService = props.streamingServices.find((ownedService) => ownedService === service);
-    
 
     //for changing how and which services can be seen in edit mode
     let displayService = () => {
@@ -34,12 +29,8 @@ const UserPageView = (props) => {
 
     //for changing which services can be edited in edit mode
     let editService = () => {
-      if(props.isEdit) {
-        if(hasService) {
-          return props.onRemoveServiceButton;
-        }
-        return props.onAddServiceButton;
-      }
+      if(props.isEdit)
+        return hasService? props.onRemoveServiceButton : props.onAddServiceButton;
     }
 
     return (
@@ -56,14 +47,20 @@ const UserPageView = (props) => {
 
   const editUsername = () => {
     if(props.isEdit) {
-      
-      return (<div>
-        <h3>Edit Username:</h3>
-        <InputText id="username" value={props.username} onChange={usernameChanged} onKeyDown={keyDown}/>
-      </div>
+      return (
+        <div>
+          <h3>Edit Username:</h3>
+          <InputText 
+            id="username" 
+            value={props.username} 
+            onChange={usernameChanged} 
+            onKeyDown={keyDown}
+          />
+        </div>
       );
     }
-    return (<div>
+    return (
+      <div>
         <h3>{props.username}</h3>
         <p className="email" >{props.useremail}</p>
       </div>);
@@ -73,7 +70,13 @@ const UserPageView = (props) => {
 
   return (
       <section className="userprofile">
-        <Button className="edituser" label={props.isEdit? "Done" : "Edit"} onClick={onToggleEditmode} size="small" outlined/>
+        <Button 
+          className="edituser" 
+          label={props.isEdit? "Done" : "Edit"} 
+          onClick={onToggleEditmode} 
+          size="small" 
+          outlined
+        />
         <h2>User Profile</h2>
         <FaUserCircle size="80" />
         {editUsername()}
