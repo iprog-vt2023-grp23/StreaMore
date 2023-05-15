0918ba93fed811418a3038c1ef8eae91d8187f29
gablan <gablan@kth.se>
2023-05-15 15:55:50 +0200

    Fixed crash when no services are available

--- src/ApiKey.jsx
+++ src/ApiKey.jsx
@@ -1,7 +1,8 @@
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "f070a4eaefmsh6a2cea4e901d65bp1dac56jsne1ade9b4f78d",
+    //"X-RapidAPI-Key": "f070a4eaefmsh6a2cea4e901d65bp1dac56jsne1ade9b4f78d",
+    "X-RapidAPI-Key": "7e8ea524f8msh9003ce878d468d6p1a6463jsn87230c74357b",
     "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
   },
 };
